b402acec06203477ce7a06a19620a62ddaa5e6f0
trungvuive <trung150502@gmail.com>
2024-06-01 11:18:55 +0700

    add logs and relationship entity

---
+++ api/src/main/java/com/trung/filemanager/entity/GroupUser.java
@@ -0,0 +1,41 @@
+package com.trung.filemanager.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class GroupUsers {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    Integer groupid;
+
+    String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(Integer groupid) {
+        this.groupid = groupid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
