a9f5ffda0410927a75c94b4359842b9731ea8073
Trung Nguyen <trung150502@gmail.com>
2024-07-02 21:54:57 +0700

    Update AwsS3Config.java

--- api/src/main/java/com/trung/filemanager/config/AwsS3Config.java
+++ api/src/main/java/com/trung/filemanager/config/AwsS3Config.java
@@ -1,5 +1,24 @@
+import org.springframework.beans.factory.annotation.Value;
+
 @Configuration
 public class AwsS3Config {
     // https://ducmanhphan.github.io/2019-02-20-Uploading-files-with-AWS-S3-spring-boot/
-    // Ref above link
+    // Ref above linK
+    @Value("${mpd.aws.access_key_id}")
+    private String awsId;
+    
+    @Value("${mpd.aws.secret_access_key}")
+    private String awsKey;
+    
+    @Value("${mpd.s3.region}")
+    private String region;
+    
+    @Bean 
+    public AmazonS3 s3client() {
+            BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsId, awsKey);
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+                                                    .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                                                    .build();
+            return s3Client;
+    }
 }
